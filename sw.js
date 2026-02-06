55fd058118fec0b3ec7c0359019aecda4b72d8da
Ale2021Git <86354193+Ale2021Git@users.noreply.github.com>
2026-02-06 17:12:00 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE_NAME = 'bbraun-scales-v2'; // Mudei para v2 para forçar a atualização
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'icon-192x192.png',
+  'icon-512x512.png',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600;800&family=JetBrains+Mono:wght@700&display=swap',
+  'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200'
+];
+// ... restante do código do service worker
+
